d0940cae016871d4c2ee21499a39d5976be81d86
spanwala-cmu <spanwala@andrew.cmu.edu>
2024-08-30 10:10:18 -0400

    Update analyze.py for creds

--- analyze.py
+++ analyze.py
@@ -2,9 +2,10 @@ from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 import time
+import creds
 
-endpoint = "ENTER ENDPOINT HERE"
-key = "ENTER KEY HERE"
+endpoint = creds.endpoint
+key = creds.key
 
 credentials = CognitiveServicesCredentials(key)
 
